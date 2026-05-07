81a910997149de3435befdfa9d67096cb562582f
Gabriel Rigoni <rigonigabriel12@gmail.com>
2026-05-07 15:33:35 -0300

    debug getRanking

--- api/GetRanking/index.js
+++ api/GetRanking/index.js
@@ -3,6 +3,11 @@ const { TableClient } = require("@azure/data-tables");
 module.exports = async function (context) {
 
     try {
+
+        context.log("Iniciando GetRanking");
+
+        context.log("STORAGE:", process.env.STORAGE);
+
         const client =
             TableClient.fromConnectionString(
                 process.env.STORAGE,
@@ -14,22 +19,31 @@ module.exports = async function (context) {
         let players = [];
 
         for await (const row of client.listEntities()) {
+
             players.push({
                 name: row.name,
                 score: row.score
             });
+
         }
 
         players.sort((a, b) => b.score - a.score);
+
         context.res = {
             status: 200,
             body: players.slice(0, 10)
         };
 
     } catch (error) {
+
         context.res = {
             status: 500,
-            body: error.message
+            body: {
+                error: error.message,
+                stack: error.stack
+            }
         };
+
     }
+
 };
